dfd13677792b6495d94a384a60980e446b85d502
Artis84 <lopsta10@gmail.com>
2022-08-28 22:27:43 +0200

    inital commit

---
+++ forum_client/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+  	<div class="jumbotron">
+      <h1 class="display-3">Bienvenue sur le Forum</h1>
+      <p class="lead">Ici vous rencontrerez des gens de type BIEN!</p>
+      <hr class="my-4">
+      <p>Pour les idée et les problemes que vous pourrez rencontrez sur le forum sa ce passe sur le GIT HUB</p>
+      <p class="lead">
+      <a class="btn btn-primary btn-lg" href="#" role="button">Inscrivez-vous</a>
+    </p>
+  </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home'
+};
+</script>
